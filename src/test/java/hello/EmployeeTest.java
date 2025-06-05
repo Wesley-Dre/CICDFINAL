8bd63af67b2851db6095080c8bf00e0d3c9458bc
wdb <wesleydb2014@gmail.com>
2025-06-05 17:46:09 -0300

    teste2

--- src/test/java/hello/EmployeeTest.java
+++ src/test/java/hello/EmployeeTest.java
@@ -14,6 +14,6 @@ public class EmployeeTest {
         assertThat(employee.getId(), is(1));
         assertThat(employee.getName(), is("John Doe"));
         assertThat(employee.getPosition(), is("Developer"));
-        assertThat(employee.toString(), containsString("John Doe"));
+        assertThat(employee.toString(), containsString("John Doe"));ssss
     }
 }
